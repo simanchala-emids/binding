e8d52ebbab9325e60e75a74826ccbdad07feb874
simanchala-emids <simanchala.pradhan@emids.com>
2018-03-18 02:02:56 +0530

    added example of two way binding using ng model

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -2,31 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `
-      <h2> Welcome {{ name }}</h2>
-      <input id="{{ myid }}" type="text" value="sim"/><br>
-      <input [id]="myId" type="text" value="sim"/><br>
-      <input disabled type="text" value="disabled"/><br>
-      <input disabled="false" type="text" value="disabled"/><br>
-      <input disabled="true" type="text" value="disabled"/><br>
-      <input disabled="{{false}}" type="text" value="disabled"/><br>
-      <input [disabled]="isDisabled" type="text" value="disabled"/><br>
-      <input bind-disabled="isDisabled" type="text" value="disabled"/>
-  `,
-  styles: [` `]
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
   public name = "Simanchala";
-  public myId = "testId";
-  public isDisabled = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  greetUser() {
-    return "Hello " + this.name;
-  }
 }
